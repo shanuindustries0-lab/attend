4c5024903413d3d3d0bf0c28dae759b6451c9548
Shanu <shanuindustries0@gmail.com>
2026-09-20 02:19:57 +0530

    Update route.ts for env var

--- src/app/api/cron/daily-backup/route.ts
+++ src/app/api/cron/daily-backup/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: Request) {
       credentials: {
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
 
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/^"|"$/g, '').replace(/\\n/g, '\n')
       },
       scopes: ["https://www.googleapis.com/auth/drive.file"],
     });
